01c46c691a224d2497f92b67466c925c65aac5aa
sheaal <alisandra.sh05@gmail.com>
2024-05-04 19:16:51 +0700

    коммит 1 стьатья

---
+++ cypress/integration/habr_cypress_test.spec.cy.js
@@ -0,0 +1,49 @@
+describe('Cypress Tests', () => {
+    if('None-existent login test', () => {
+        cy.fixture('cypressTests').then(data => {
+            cy.log('Переход на страницу авторизации')
+            cy.visit(data.main_url)
+
+            cy.log('Вход несуществующего логина')
+            cy.get('input[id="passp-field-login"]')
+                .type(data.none_existent_login)
+
+            cy.log('Клик по кнопке "Войти"')
+            cy.get('div[class="passp-button passp-sign-in-button"] button[type="submit"]')
+                .click()
+            
+            cy.log('Проверка что появился элемент сигнализирующий об ошибке')
+            cy.get('div[class="passp-form-field__error"]')
+                .should('exist')
+        })
+    })
+
+    if('None-existent password test', () => {
+        cy.fixture('cypressTests').then(data => {
+            cy.log('Переход на страницу авторизации')
+            cy.visit(data.main_url)
+
+            cy.log('Ввод логина')
+            cy.get('input[id="passp-field-login"]')
+                .type(data.login)
+
+            cy.log('Клик по кнопке "Войти"')
+            cy.get('div[class="passp-button passp-sign-in-button"] button[type="submit"]')
+                .click()
+            // ожидаем 2 секунды для появления формы ввода пароля
+            cy.wait(2000);
+
+            cy.log('Вход несуществующего пароля')
+            cy.get('input[id="passp-field-passwd"]')
+                .type(data.none_existent_password)
+
+            cy.log('Клик по кнопке "Войти"')
+            cy.get('div[class="passp-button passp-sign-in-button"] button[type="submit"]')
+                .click()
+            
+            cy.log('Проверка что появился элемент сигнализирующий об ошибке')
+            cy.get('div[class="passp-form-field__error"]')
+                .should('exist')
+        });
+    });
+});
